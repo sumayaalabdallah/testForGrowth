eeb5f125a17f5dbf893f43dabb1b436f4ac0b2ef
sumayaalabdallah <sumayaalabdallah@gmail.com>
2024-09-21 18:23:48 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+  // Add a click event on each of them
+  $navbarBurgers.forEach((el) => {
+    el.addEventListener("click", () => {
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle("is-active");
+      $target.classList.toggle("is-active");
+    });
+  });
+});
+
+const thumbnails = document.querySelectorAll('.thumb');
+const mainImage = document.getElementById('main-image');
+
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', function() {
+        mainImage.src = this.src;
+    });
+});
+
+//FAQ
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
